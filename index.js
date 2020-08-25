5aec7cae26ac39318bbe6cfff88f3ded16ce641e
Paul Wittchen <wittchenpaul@gmail.com>
2020-08-25 16:50:20 +0200

    drumkit workaround used

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
 window.addEventListener('keydown', (event) => {
-    console.log(event);
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add('active')
+    audio.play();
+    audio.currentTime = 0;
+    setTimeout(function(){ 
+        key.classList.remove('active')
+    }, 150);
 });
